84a1b0cff574e11e38c0a0785ebad4456b19ee87
Thomas Dy <thatsmydoing@gmail.com>
2016-03-17 19:48:41 +0800

    Add woff2 in loader config

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
             { test: /\.jsx$/, loaders: ["react-hot", "jsx"] },
 
             // for bootstrap stuff
+            { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,  loader: "url?limit=10000&mimetype=application/font-woff" },
             { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,   loader: "url?limit=10000&mimetype=application/font-woff" },
             { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=application/octet-stream" },
             { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,    loader: "file" },
